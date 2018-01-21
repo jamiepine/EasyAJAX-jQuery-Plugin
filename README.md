68e8b734ea3e26103cf4f9db15b62d8cfe4d1148
jamespine <32987599+jamespine@users.noreply.github.com>
2018-01-21 20:42:31 +0000

    jeff

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # EasyAJAX jQuery Plugin
+
+An incredibly useful jQuery plugin to implement AJAX loading / form submission into any website or app, with full support for CSRF tokens. EasyAJAX uses HTML markup and tags to add AJAX to any link / form. 
+
+Check out the **[demo](http://jamiepine.github.com/)** to see it in action!
+
+### Installation
+
+Make sure you have the latest version of jQuery installed and called before this plugin. Insert the following into your `<head>` tag, just like any other jQuery script.
+
+```html
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/jquery.easyajax.min.js"></script>
+```
+### Activate a link
---
+++ jquery.ajaxpost.js
@@ -0,0 +1,128 @@
+/*!
+ * jquery.easyajax.js v1.0 (http://jamiepine.github.io/jquery.easyajax.js/)
+ * @copyright 2018 Jamie Pine
+ */
+$(document).on('click', '[ajax-enable]', function(e) {
+
+    if ($(this)[0].tagName != 'FORM') {
+
+        e.preventDefault();   console.log("l");
+
+        var l = '#' + $(this).attr('id');;
+        var f = false;
+
+        prepareAjaxObject(l, f);
+    }
+});
+$(document).on('submit', 'form', function(e) {
+
+    if ($(this).is("[ajax-enable]")) {
+
+        e.preventDefault();   console.log("f");
+
+        var f = '#' + $(this).attr('id');
+        var l = false;
+
+        prepareAjaxObject(l, f);
+    }
+});
+function prepareAjaxObject(l, f) {
+
+    var ad = {}
+
+    if (f !== false) {
+
+        ad['formElements'] = [];
+
+        $(f).find('input, select').each(function() {
+
+            var eid = $(this).attr('id');
+            var eva = $(this).val();
+
+            var eO = {};
+
+            eO['key'] = eid;
+            eO['val'] = eva;
+
+            ad.formElements.push(eO);
+
+        });
+        var id = f;
+    }
+
+    if (l !== false) {
+
+        var id = l;
+    }
+    if ($(id).is("[ajax-action]")) {
+
+        var aa = $(id).attr('ajax-action');
+
+        ad['ajaxAction'] = aa;
+
+    } else {    alert("Error: ajax-action required");
+    }
+    if ($(id).is("[ajax-success]")) {
+
+        var aSa = $(id).attr('ajax-success');
+
+        ad['ajaxSucsessAction'] = aSa;
+    }
+    if ($(id).is("[csrf-token]")) {
+
+        var csrf = $(id).attr('csrf-token');
+
+        ad['_token'] = csrf;
+
+    } else if (document.querySelectorAll('meta[name="csrf-token"]').length > 0) {
+
+        var csrf = $('meta[name="csrf-token"]').attr('content');
+
+        ad['_token'] = csrf;
+    }
+
+    console.log(ad);
+
+    ajaxSend(ad);
+}
+
+function ajaxSend(ad) {
+
+    $.ajax({
+        type: "POST",
+        url: ad["ajaxAction"],
+        data: ad,
+        success: function(response) {
+            if (response) {
+                ajaxSuccess(response, ad);
+            }
+        },
+        error: function(response) {
+            console.log("ERROR");
+        }
+    });
+
+};
+
+function ajaxSuccess(response, ad) {
+
+  var sa = ad["ajaxSucsessAction"];
+
+  if (sa == "msg") {
+    //do something (finish later, night)
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
